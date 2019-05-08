65417809cc1202fd6e7a0ce144dbe5cf7a0cd87f
Dan <daniel@accolade.es>
2019-05-08 11:24:56 +0100

    local--01 Module:Added top menu template with layout and Block model(xml,php) -ragedleinad

---
+++ Debug/Block/Debugger.php
@@ -0,0 +1,10 @@
+<?php
+namespace Ultimate\Debug\Block;
+
+class Debugger extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+}
